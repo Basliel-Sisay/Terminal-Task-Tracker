73cbb3530683572dee3466d4c71c58e20a7a395a
Basliel Sisay <basliel.ugr-3563-16@aau.edu.et>
2026-04-09 13:32:01 +0300

    create CLI interface with argument parsing and error handling

---
+++ app.js
@@ -0,0 +1,29 @@
+const manager = require('./manager');
+
+const command = process.argv[2]; 
+const value = process.argv[3];
+try{
+if(command.toLowerCase() === 'add'){
+    manager.addTask(value);
+    console.log("Your "+ value+"is added successfully");
+}
+else if(command.toLowerCase() === 'list'){
+    manager.listTask();
+}
+else if(command.toLowerCase() === 'done'){
+    manager.toggleTask(Number(value));
+    console.log("Status Update for task: " + value);
+}
+else if(command.toLowerCase() === 'remove'){
+    manager.remove(Number(value));
+    console.log("Deleted task: "+ value);
+}
+else {
+    console.log("Please enter a valid command and try again later");
+}
+} 
+catch(error){
+    console.error("Error command: " + error.message);
+}
+
+console.log(process.argv);
